686bd208276cb62ae706968c030ae71303c4da2e
Flávio Gomes da Silva Lisboa <flavio.lisboa@serpro.gov.br>
2018-04-12 11:17:42 -0300

    Revert "Avoid context error #2"
    This reverts commit 15fbc6613c3cb2d05bec2dbb589bdaa49a3b9c64.

--- src/Fgsl/Eyedatagrid/EyeDataGrid.php
+++ src/Fgsl/Eyedatagrid/EyeDataGrid.php
@@ -45,7 +45,6 @@ class EyeDataGrid
     private $select_where = ''; // Where clause
     private $select_table = ''; // Table to read
     private $image_path = ''; // Path to images
-    private static $instance = false;
 
 
      // Filename of required images
@@ -117,8 +116,6 @@ class EyeDataGrid
             list($column, $value) = $this->parseInputCond($filter);
             $this->setFilter($column, $value);
         }
-
-        self::$instance = true;
     }
 
     /**
@@ -889,7 +886,7 @@ class EyeDataGrid
     */
     public function printJavascript()
     {
-        if (self::$instance)
+        if ($this)
         {
             $page = $this->page;
             $order = (($this->order) ? implode(':', $this->order) : '');
